b2cdca83e909c06d8dfbd0efce352fcfe2438ea3
jugstalt <gstalt@gmx.at>
2023-04-03 17:14:55 +0200

    added min.js

--- _dist.bat
+++ _dist.bat
@@ -1,7 +1,7 @@
 @echo off
 
-terser .\src\jExt.js -c -m -o dist/jExt.min.js
+terser .\src\jExt.js -c -m -o dist/jext.min.js
 
-terser .\src\jExt.js .\src\jExt-ext.js -c -m -o dist/jExt-ext.min.js
+terser .\src\jExt.js .\src\jExt-ext.js -c -m -o dist/jext-ext.min.js
 
-terser .\src\jExt.js .\src\jExt-ext.js .\src\jExt-ajax.js -c -m -o dist/jExt-full.min.js
+terser .\src\jExt.js .\src\jExt-ext.js .\src\jExt-ajax.js -c -m -o dist/jext-full.min.js
